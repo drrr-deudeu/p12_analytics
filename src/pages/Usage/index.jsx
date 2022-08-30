c1b38c5f804d12ddb6a067ef4669b8e695a776fb
drrr-deudeu <denis.rouiller@gmail.com>
2022-08-30 10:46:57 +0200

    Link to Fake User

--- src/pages/Usage/index.jsx
+++ src/pages/Usage/index.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
 function Usage() {
   return (
-    <div>
+    <div className='usage'>
       <Link to='/user/12'>goto user 12</Link>
       <Link to='/user/18'>goto user 18</Link>
+      <Link to='/user/1'>goto user 1</Link>
     </div>
   )
 }
